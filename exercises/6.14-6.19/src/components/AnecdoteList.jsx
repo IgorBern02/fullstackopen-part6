0847b44513a4a68c13fb86d5757e7580c864a431
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-04-09 15:11:17 -0300

    feat: exercise 6.14-6.19

---
+++ exercises/6.14-6.19/src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state.anecdotes);
+  const filter = useSelector((state) => state.filter);
+
+  const filteredAnecdotes = anecdotes.filter((anecdote) =>
+    anecdote.content.toLowerCase().includes(filter.toLowerCase()),
+  );
+  const dispatch = useDispatch();
+
+  const handleVote = (id) => {
+    dispatch(voteAnecdote(id));
+  };
+
+  return (
+    <div>
+      {filteredAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
