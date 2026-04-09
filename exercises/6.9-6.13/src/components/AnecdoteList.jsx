2a280bffe7b97aa635d287f21858f01e9c1f1899
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-04-09 15:10:17 -0300

    feat: exercises 6.9-6.13

---
+++ exercises/6.9-6.13/src/components/AnecdoteList.jsx
@@ -0,0 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
+import { setNotificationWithTimeout } from "../reducers/notificationReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state.anecdotes);
+  const filter = useSelector((state) => state.filter || "");
+
+  const state = useSelector((state) => state);
+  console.log(state);
+
+  const filteredAnecdotes = anecdotes
+    .filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase()),
+    )
+    .sort((a, b) => b.votes - a.votes);
+
+  const dispatch = useDispatch();
+
+  const handleVote = (anecdote) => {
+    dispatch(voteAnecdote(anecdote.id));
+    dispatch(
+      setNotificationWithTimeout(`you voted '${anecdote.content}'`, 5000),
+    );
+  };
+
+  return (
+    <div>
+      {filteredAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
