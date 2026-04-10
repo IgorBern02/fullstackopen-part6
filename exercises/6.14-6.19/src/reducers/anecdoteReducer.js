a529ae9c50d7c7e6cfc0232ebf8882b6052bc365
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-04-10 13:41:03 -0300

    feat: exercise 6.16

--- exercises/6.14-6.19/src/reducers/anecdoteReducer.js
+++ exercises/6.14-6.19/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdotesAtStart = [];
 
@@ -35,7 +36,15 @@ const anecdoteSlice = createSlice({
   },
 });
 
-export const { voteAnecdote, createAnecdote, setAnecdotes } =
-  anecdoteSlice.actions;
+const { setAnecdotes } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions;
 
 export default anecdoteSlice.reducer;
